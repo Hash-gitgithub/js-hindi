8bb1e60b2e6376ef8c1b216fcbb48e4c473ea861
Hash-gitgithub <hashiransari008@gmail.com>
2024-06-19 06:44:29 +0000

    conver

---
+++ 01_basics/conversion.js
@@ -0,0 +1,26 @@
+let score = false
+console.log(typeof score);
+console.log(typeof (score));
+
+let value = Number(score);
+console.log(typeof value);
+console.log(value);
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0
+
+let islog = "n"
+
+let booleanislog = Boolean (islog);
+console.log(booleanislog);
+
+// 1=> true; 0 => false
+//"" => false
+// "any" => true
+
+let sonenum = 33
+
+let somenum = String(sonenum);
+console.log(somenum);
+console.log(typeof somenum);
